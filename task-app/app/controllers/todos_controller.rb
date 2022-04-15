7d91fe9c5fbdf60d9704e98246a2ff5f8d28dc91
Jubel13 <jubelsinaga13@gmail.com>
2022-04-15 17:47:05 +0700

    Add association to todo

--- task-app/app/controllers/todos_controller.rb
+++ task-app/app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: %i[ show edit update destroy ]
+  before_action :set_todo, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy]
 
   # GET /todos or /todos.json
   def index
@@ -7,25 +9,25 @@ class TodosController < ApplicationController
   end
 
   # GET /todos/1 or /todos/1.json
-  def show
-  end
+  def show; end
 
   # GET /todos/new
   def new
-    @todo = Todo.new
+    # @todo = Todo.new
+    @todo = current_user.todos.build
   end
 
   # GET /todos/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /todos or /todos.json
   def create
-    @todo = Todo.new(todo_params)
+    # @todo = Todo.new(todo_params)
+    @todo = current_user.todos.build(todo_params)
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
+        format.html { redirect_to todo_url(@todo), notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +40,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully updated." }
+        format.html { redirect_to todo_url(@todo), notice: 'Todo was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +54,25 @@ class TodosController < ApplicationController
     @todo.destroy
 
     respond_to do |format|
-      format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
+      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params[:id])
-    end
+  def correct_user
+    @todo = current_user.todos.find_by(id: params[:id])
+    redirect_to todos_path, notice: 'Not Authorized to edit this todo' if @todo.nil?
+  end
 
-    # Only allow a list of trusted parameters through.
-    def todo_params
-      params.require(:todo).permit(:task, :due, :priority, :completed)
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def todo_params
+    params.require(:todo).permit(:task, :due, :priority, :completed, :user_id)
+  end
 end
--- task-app/db/schema.rb
+++ task-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_162423) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_100730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_162423) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
