b4e77b1034348e3d0839a261097dc3927a85423b
Jubel13 <jubelsinaga13@gmail.com>
2022-04-15 18:13:37 +0700

    Edit style

--- task-app/config/routes.rb
+++ task-app/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :todos
   devise_for :users
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
+  root 'todos#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
